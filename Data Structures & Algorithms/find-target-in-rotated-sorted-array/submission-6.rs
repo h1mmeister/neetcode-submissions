6186f86b3bd849a7d1604aabc17d63ae6c7a962c
Himanshu <69789970+h1mmeister@users.noreply.github.com>
2026-05-24 20:33:14 +0200

    Add: find-target-in-rotated-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-6.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+
+        while left <= right {
+            let mut mid = left + (right - left) / 2;
+
+            if target == nums[mid] {
+                return mid as i32;
+            } else if nums[left] <= nums[mid] {
+                if nums[left] <= target && target < nums[mid] {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if nums[mid] < nums[right] {
+                if nums[mid] < target && target <= nums[right] {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
