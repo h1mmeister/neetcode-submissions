37eba860e10fe9589557cba469961f00cc1ef082
Himanshu <69789970+h1mmeister@users.noreply.github.com>
2026-08-02 15:24:44 +0200

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        one = 1
+        two = 2
+
+        for i in range(n - 2):
+            one, two = two, one + two
+
+        return two
+
+        
