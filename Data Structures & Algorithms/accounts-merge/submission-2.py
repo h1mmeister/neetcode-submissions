250649b19bba91b83202f751743b9943b2c32d5f
Himanshu <69789970+h1mmeister@users.noreply.github.com>
2026-08-16 15:29:19 +0200

    Add: accounts-merge - submission-2

---
+++ Data Structures & Algorithms/accounts-merge/submission-2.py
@@ -0,0 +1,53 @@
+class UnionFind:
+
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.size = [0] * n
+
+    def find(self, value):
+        while value != self.parents[value]:
+            self.parents[value] = self.parents[self.parents[value]]
+            value = self.parents[value]
+        return value
+
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+
+        if root_a == root_b:
+            return False
+
+        if self.size[root_a] < self.size[root_b]:
+            root_a, root_b = root_b, root_a
+
+        self.parents[root_b] = root_a
+        self.size[root_a] += self.size[root_b]
+
+        return True
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        email_to_account = {}
+
+        for idx, account in enumerate(accounts):
+            for email in account[1:]:
+                if email in email_to_account:
+                    uf.union(idx, email_to_account[email])
+                else:
+                    email_to_account[email] = idx
+
+        email_groups = defaultdict(list)
+
+        for email, idx in email_to_account.items():
+            leader = uf.find(idx)
+            email_groups[leader].append(email)
+
+        result = []
+
+        for leader, emails in email_groups.items():
+            name = accounts[leader][0]
+            result.append([name] + sorted(emails))
+
+        return result
