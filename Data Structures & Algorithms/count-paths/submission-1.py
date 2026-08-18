44835851404c9cd3dd62521dbaf2f5b21ea6eb2a
Himanshu <69789970+h1mmeister@users.noreply.github.com>
2026-08-18 13:20:00 +0200

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        cache = [[0 for _ in range(n)]for _ in range(m)]
+
+        def unique_path_helper(row, col):
+            if row == m-1 and col == n-1:
+                return 1
+            elif row >= m or col >= n:
+                return 0
+            if cache[row][col] == 0:
+                right = unique_path_helper(row, col + 1)
+                down = unique_path_helper(row + 1, col)
+                cache[row][col] = right + down
+
+            return cache[row][col]
+        return unique_path_helper(0,0)
+        
